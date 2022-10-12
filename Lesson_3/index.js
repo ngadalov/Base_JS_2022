3aaa4d2a7342063a8f900c7f59ac5cb50a598d42
Nikolay <ngadaloff@yandex.ru>
2022-10-12 16:03:10 +0300

    11.10.22

---
+++ Lesson_3/index.js
@@ -0,0 +1,42 @@
+// const name = "Alex";
+
+
+// const numberName = Number(name);
+
+// console.log(numberName);
+
+// const age = 26;
+
+// const ageToString = String(age);
+
+// console.log(typeof ageToString);
+
+// const nullValue = null;
+
+// console.log(Number(nullValue));
+
+//Логическое преобразование 
+// 0, null, undefined, Nan, "" становится fals
+//Любое другое значение становится true
+
+// console.log(Boolean(""));
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean({}));
+
+//Исключение 
+// undefined при численном преобразовании становится NaN а не 0
+
+//Унарный плюс
+// console.log(typeof +"45");
+// console.log(typeof Number('45'));
+//Бинарный плюс
+//Конкатенацию строк склейка значений если одно из них сторка
+
+//console.log('3' == 3); //true
+console.log(5 > 4);
+console.log('2' > '12');
+
+
+
+
